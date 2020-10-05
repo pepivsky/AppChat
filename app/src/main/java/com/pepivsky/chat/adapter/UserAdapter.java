eecaadd109496161b2d1f5dfe50e0bee99e2531f
Pepivsky <ironglas68@gmail.com>
2020-10-05 01:17:31 -0500

    Cambio de activity principal y logica para la navegacion entre activities

--- app/src/main/java/com/pepivsky/chat/adapter/UserAdapter.java
+++ app/src/main/java/com/pepivsky/chat/adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.pepivsky.chat.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.pepivsky.chat.R;
 import com.pepivsky.chat.model.User;
+import com.pepivsky.chat.view.ChatActivity;
 
 import java.util.ArrayList;
 
@@ -41,8 +43,11 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> { 
         holder.goToChatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "Chateando con el usuario" + currentUser.getUsername(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Chateando con el usuario " + currentUser.getUsername(), Toast.LENGTH_SHORT).show();
                 //Intent con extras
+                Intent intent = new Intent(context, ChatActivity.class);
+                intent.putExtra("user", currentUser.getUsername());
+                context.startActivity(intent); //lanza la actividad del chat
             }
         });
     }
--- app/src/main/java/com/pepivsky/chat/view/ChatActivity.java
+++ app/src/main/java/com/pepivsky/chat/view/ChatActivity.java
@@ -1,35 +1,135 @@
 package com.pepivsky.chat.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.pepivsky.chat.R;
+import com.pepivsky.chat.adapter.MessageAdapter;
 import com.pepivsky.chat.adapter.UserAdapter;
+import com.pepivsky.chat.model.Message;
 import com.pepivsky.chat.model.User;
+import com.pepivsky.chat.pubnub.PUBNUB;
+import com.pepivsky.chat.pubnub.utils.UserPresenceCallback;
+import com.pepivsky.chat.view.ContactDetailActivity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ChatActivity extends AppCompatActivity {
-    RecyclerView contactsList;
-    UserAdapter userAdapter;
+    FloatingActionButton sendButton;
+    EditText edtMessage;
+    RecyclerView messagesList;
+    MessageAdapter adapter;
+    TextView tvUserNameToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        contactsList = findViewById(R.id.contactRecyclerList);
-        contactsList.setHasFixedSize(true);
+        //toolbar
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        //components
+        sendButton = findViewById(R.id.fabSend);
+        edtMessage = findViewById(R.id.edtMessage);
+        tvUserNameToolbar = findViewById(R.id.tvUsernameToolbar);
 
-        contactsList.setAdapter(userAdapter);
+        messagesList = findViewById(R.id.recyclerMessages);
+        messagesList.setHasFixedSize(true);
+        adapter = new MessageAdapter(new ArrayList<Message>());
+        messagesList.setAdapter(adapter);
 
-        userAdapter = new UserAdapter(dummyUsers(), this);
+        //obtener los extras
+        Bundle extras = getIntent().getExtras();
+        //setear el username
+        tvUserNameToolbar.setText(extras.getString("user"));
+
+
+        PUBNUB.getInstance().connect(success -> Log.d("PUBNUB -> UserConnected", success.toString()), new UserPresenceCallback() {
+            @Override
+            public void userJoin(User user) {
+                Log.d("PUBNUB -> UserJoin", user.getUsername());
+            }
+
+            @Override
+            public void userLeave(User user) {
+                Log.d("PUBNUB -> User Leave", user.getUsername());
+            }
+        });
+
+        PUBNUB.getInstance().setMessagesCallback(message -> {
+            Log.d("PUBNUB", message.getContent());
+            runOnUiThread(() -> adapter.add(message));
+        });
+
+
+        sendButton.setOnClickListener(v -> {
+            PUBNUB.getInstance().publish(new Message(edtMessage.getText().toString(), PUBNUB.MY_UUID));
+            edtMessage.setText("");
+        });
+
     }
 
-    private ArrayList<User> dummyUsers() {
-        ArrayList<User> users = new ArrayList<>();
-        users.add(new User("Alex"));
-        return users;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PUBNUB.getInstance().unsuscribe();
     }
+
+    /*private ArrayList<Message> dummyMessages() {
+        ArrayList<Message> messages = new ArrayList<>();
+
+        messages.add(new Message("Hola", true));
+        messages.add(new Message("Hola", false));
+        messages.add(new Message("como te va?", true));
+        messages.add(new Message("bien :), y a ti?", false));
+
+
+        return messages;
+    }*/
+
+
+    //Creando menu de la toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ver_contacto:
+                Intent intent = new Intent(this, ContactDetailActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
 }
